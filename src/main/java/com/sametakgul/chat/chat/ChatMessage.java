26f093cf635b3952086473c5a9ce0f12e9656984
Muhammed Samet Akgul <msametakgul@gmail.com>
2024-02-28 15:53:59 +0300

    Initial commit

---
+++ src/main/java/com/sametakgul/chat/chat/ChatMessage.java
@@ -0,0 +1,16 @@
+package com.sametakgul.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+
+}
---
+++ src/main/java/com/sametakgul/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.sametakgul.chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
